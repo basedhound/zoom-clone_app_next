726edbf6e9bd2dd6cf8788b4b8461cd0f93eeb20
Jozef S <franck.vukelic@gmail.com>
2024-04-16 15:44:36 +0700

    Build route groups and dynamic routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      Navbar
+      {children}
+      Footer
+    </main>);
+};
+
+export default RootLayout;

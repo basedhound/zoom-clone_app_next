76bcce83d3aa68c64fddb8d5d2057c261c746c02
Jozef S <franck.vukelic@gmail.com>
2024-04-16 12:06:59 +0700

    Add new files and update configurations

---
+++ components/CallList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallList = () => {
+  return (
+    <div>CallList</div>
+  )
+}
+
+export default CallList
---
+++ components/EndCallButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EndCallButton = () => {
+  return (
+    <div>EndCallButton</div>
+  )
+}
+
+export default EndCallButton

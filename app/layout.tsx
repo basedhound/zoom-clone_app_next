ab94e366005b02ee5e4f3e191f5d179d71925350
Jozef S <franck.vukelic@gmail.com>
2024-04-19 17:55:51 +0700

    Add: Upcoming meeting type

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+
 // =======================
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -75,6 +75,9 @@ const MeetingTypeList = () => {
     }
   };
 
+  // get meetingLink
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetail?.id}`;
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -147,13 +150,12 @@ const MeetingTypeList = () => {
           title="Meeting Created"
           className="text-center"
           handleClick={() => {
-            // navigator.clipboard.writeText(meetingLink);
-            // toast({ title: "Link Copied" });
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: "Link Copied" });
           }}
           image={"/icons/checked.svg"}
           buttonIcon="/icons/copy.svg"
           buttonText="Copy Meeting Link"
-          // className="text-center"
         />
       )}
 
